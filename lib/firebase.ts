b0ab3f337f8873712f5bb2257494e4235b18e2b0
Anjali <anjali.12114710@lpu.in>
2024-09-08 21:18:58 +0530

    patched security vulnerabilities

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBoTAke9k-SBFutjniBDO6hgzyLePPFv8E',
-  authDomain: 'taskmanager-6f422.firebaseapp.com',
-  projectId: 'taskmanager-6f422',
-  storageBucket: 'taskmanager-6f422.appspot.com',
-  messagingSenderId: '1031620678994',
-  appId: '1:1031620678994:web:505c04e1b44ef6b222d68b'
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
