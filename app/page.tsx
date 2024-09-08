2b257e04ed614b3e42f9cd81a3e563584a647401
Anjali <anjali.12114710@lpu.in>
2024-09-08 21:48:10 +0530

    patched security vulnerabilities

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 import { getTasks } from '../utils/taskOperations';
 import TaskBoard from '../components/TaskBoard';
 
+
 const Page = async () => {
   const tasks = await getTasks();
 
   return (
     <div className="container mx-auto p-4">
-      <TaskBoard tasks={tasks} />
+      <TaskBoard/>
     </div>
   );
 };
